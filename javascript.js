493b3e60405bf495bc21ef74c779fb93cc07d68e
re-vc <frederikrehovic@gmail.com>
2023-02-02 23:41:00 +0100

    got working, used innerHTML and .join

--- javascript.js
+++ javascript.js
@@ -7,15 +7,21 @@
 //     }
 // }
 
+// let mystring = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
+// console.log(mystring);
+// console.log(mystring.replaceAll('$', ' '));
+
 let myArray = [];
+
+function checkArray() {
+    document.getElementById("displayArray").innerHTML = myArray.join(', ');
+}
 
 function addToArray() {
     let myNumber = document.getElementById("myNumber").value;
     if (myNumber != null) {
         myArray.push(myNumber);
-        console.log(myArray);
-        let display = document.getElementById("display").value;
-        display = myArray;
+        checkArray();        
     }
 }
 
@@ -23,10 +29,6 @@ function subFromArray() {
     let myNumber = document.getElementById("myNumber").value;
     if (myArray.includes(myNumber)) {
         myArray.splice(myArray.indexOf(myNumber), 1);
-        console.log(myArray);
+        checkArray();        
     }
 }
-
-let mystring = "Properties,$a$set$of$immutable$values,$are$passed$to$a$component's$renderer$as$properties$in$its $HTML$tag.$A$component$cannot$directly$modify$any$properties$passed$to$it,$but$can$be$passed$ callback$functions$that$do$modify$values.$This$mechanism's$promise$is$expressed$as$properties $flow$down;$actions$flow$up";
-console.log(mystring);
-console.log(mystring.replaceAll('$', ' '));
